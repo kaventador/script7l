89f63010e3ecda6f675ca335bf8ccb15fb7d989c
kasra <kasranikshabani@yahoo>
2023-11-19 11:45:55 +0330

    finallPassword

--- script/chapter_1/main.py
+++ script/chapter_1/main.py
@@ -1,6 +1,7 @@
 import string
 from random import choices, choice
 import sys
+import argparse
 
 def create_password(length=8, upper=False, lower=False, digit=False, punc=False):
     pool = ''
@@ -19,12 +20,16 @@ def create_password(length=8, upper=False, lower=False, digit=False, punc=False)
 
 
 if __name__ == '__main__':
-    try:
-        length = (int(sys.argv[1]))
-    except ValueError :
-        print('Enter Integer')
-    else:
-        print(create_password(length))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('length',type=int , help='Length of password')
+    parser.add_argument('-u','--upper', help='use upper case',action='store_true')
+    parser.add_argument('-l','--lower', help='use lower case',action='store_true')
+    parser.add_argument('-d','--digit', help='use digit case',action='store_true')
+    parser.add_argument('-p','--punc', help='use punc case',action='store_true')
+
+    args = parser.parse_args()
+    # print(args)
+    print(create_password(args.length,args.upper,args.lower,args.digit,args.punc))
     # print(create_password(20, upper=True))
     # print(create_password(lower=True))
     # print(create_password(digit=True, upper=True))
